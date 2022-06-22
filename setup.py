e3431c91b1b7ce6a9d07e4afee46ade35d2f1542
Destination2Unknown <92536730+Destination2Unknown@users.noreply.github.com>
2022-06-22 12:54:11 +0100

    Place holder added

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pytunelogix',
-    version='1.1.4',
+    version='1.1.5',
     license='MIT',
     author="Destination2Unknown",
     author_email='destination0b10unknown@gmail.com',
