84515b6e6e7003d2f4867a9a791c9bdf28fbdb9a
Destination2Unknown <92536730+Destination2Unknown@users.noreply.github.com>
2022-07-18 20:56:20 +0100

    UpRev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pytunelogix',
-    version='1.1.13',
+    version='1.1.14',
     license='MIT',
     author="Destination2Unknown",
     author_email='destination0b10unknown@gmail.com',
